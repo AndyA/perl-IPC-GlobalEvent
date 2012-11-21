d0b5efa755b3dbe906f611ef54f31fd894aeddfe
Andy Armstrong <andy@hexten.net>
2012-11-21 17:01:32 +0000

    Hack out XS version.
    Basic working (but nasty) pure Perl version.

--- lib/IPC/GlobalEvent.pm
+++ lib/IPC/GlobalEvent.pm
@@ -4,8 +4,14 @@ require 5.008;
 
 use strict;
 use warnings;
-use Carp;
-use base qw( DynaLoader Exporter );
+
+use Carp qw( croak );
+use Linux::Inotify2;
+use Time::HiRes qw( time sleep );
+
+use base qw( Exporter );
+
+our $VERSION = '0.02';
 
 our @EXPORT_OK = qw( eventsignal eventwait );
 
@@ -15,9 +21,32 @@ IPC::GlobalEvent - System wide event notification
 
 =cut
 
-BEGIN {
-  our $VERSION = '0.06';
-  bootstrap IPC::GlobalEvent $VERSION;
+sub eventsignal {
+  my ( $file, $sn ) = @_;
+  my $tmp = "$file.tmp";
+  {
+    open my $fh, '>', $tmp or croak "Can't write $tmp: $!";
+    print $fh "$sn\n";
+  }
+  rename $tmp, $file or croak "Can't rename $tmp as $file: $!";
+}
+
+sub _read_file {
+  my $file = shift;
+  open my $fh, '<', $file or return undef;
+  chomp( my $sn = <$fh> );
+  return $sn;
+}
+
+sub eventwait {
+  my ( $file, $sn, $timeout ) = @_;
+  my $deadline = time + $timeout / 1000;
+  while ( time < $deadline ) {
+    my $nsn = _read_file( $file );
+    return $nsn if defined $nsn && $nsn ne $sn;
+    sleep 0.1;
+  }
+  return $sn;
 }
 
 1;
